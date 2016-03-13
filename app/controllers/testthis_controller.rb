44392660ea9b733aae394b85dbf1ce1da4b44735
Nicholas Gray <nicholas_gray@live.com>
2016-03-13 17:28:54 -0400

    Added Testthis Controller to home

---
+++ app/controllers/testthis_controller.rb
@@ -0,0 +1,4 @@
+class TestthisController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'testthis/home'
+
   get 'aboutus/about'
 
   get 'welcome/index'
